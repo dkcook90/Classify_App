b7918a4b486554564b7f230a586f58135f510449
fonald <dkcook90@gmail.com>
2022-04-15 14:31:50 -0600

    Setting up repo directories

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());

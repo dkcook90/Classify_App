47490b003c8c0a810933c537fe9353f511868bfa
LilTzar <eeoerkeedu@gmail.com>
2022-04-18 17:00:00 -0600

    added jwt authentication to serverside

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,41 +1,55 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-type User {
-    _id: ID!
-    username: String!
-    email: String!
-    password: String!
-    role: String!
-}
-type Student {
-    _id: ID!
-    name: String!
-    grade: Int!
-    note: String
-    
-}
-type Teacher {
-    _id: ID!
-    name: String!
-    department: String!
-    office: String!
-    students: [Student]
-}
-type School {
-    _id: ID!
-    name: String!
-    principle: String!
-    department:[Department]
-    teachers: [Teacher]
-}
-type Department {
-    _id: ID!
-    department: String!
-}
-type Query {
-    schools: [School]!
-}
+	type User {
+		_id: ID!
+		username: String!
+		email: String!
+		password: String!
+		role: String!
+	}
+
+	type Student {
+		_id: ID!
+		name: String!
+		grade: Int!
+		note: String
+	}
+
+	type Teacher {
+		_id: ID!
+		name: String!
+		department: String!
+		office: String!
+		students: [Student]
+	}
+
+	type School {
+		_id: ID!
+		name: String!
+		principle: String!
+		department: [Department]
+		teachers: [Teacher]
+	}
+
+	type Department {
+		_id: ID!
+		department: String!
+	}
+
+	type Query {
+		schools: [School]!
+	}
+
+	type Auth {
+		token: ID!
+		user: User
+	}
+
+	type Mutation {
+		addUser(username: String!, email: String!, password: String!): Auth
+		login(email: String!, password: String!): Auth
+	}
 `;
 
 module.exports = typeDefs;
--- server/utils/mutations.js
+++ server/utils/mutations.js
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-	mutation login($email: String!, $password: String!) {
-		login(email: $email, password: $password) {
+	mutation login($username: String!, $password: String!) {
+		login(username: $username, password: $password) {
 			token
 			user {
 				_id

c7d6933e00c7bb14bbdf069f8ba18a2458f1af34
LilTzar <eeoerkeedu@gmail.com>
2022-04-18 14:19:00 -0600

    adding auth 0

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";

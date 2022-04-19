f84a061a6846a56af3c18c2a12252e9903cd6cd0
LilTzar <eeoerkeedu@gmail.com>
2022-04-19 14:06:09 -0600

    added queries util, and working on schools page

--- client/src/componets/School/School.js
+++ client/src/componets/School/School.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
 import { Auth } from "../../utils/auth";
-import {} from "../../utils/mutation";
+import { QUERY_SCHOOL } from "../../utils/queries";
+import { ADD_SCHOOL, UPDATE_SCHOOL, REMOVE_SCHOOL } from "../../utils/mutation";
 
 function Schools() {
 	return (
---
+++ client/src/utils/queries.js
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_USER = gql`
+	query user($_id: Number!) {
+		user(_id: $_id) {
+			_id
+			username
+			email
+			role
+		}
+	}
+`;
+
+export const QUERY_SCHOOL = gql`
+	query school($name: String!) {
+		school(name: $name) {
+			_id
+			name
+			principle
+			budget
+			department
+			teachers
+		}
+	}
+`;

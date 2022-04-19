102762aeb05dd23b02a7a9ad7acc8c1e84c419fd
Michael Bee <bee.michael.02@gmail.com>
2022-04-19 14:17:58 -0500

    Added CSS files, off-canvas hamburger nav for navbar.

--- client/src/componets/Login/Login.js
+++ client/src/componets/Login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
+import "./Login.css";
 
 function Login() {
 	return (
--- client/src/componets/Navbar/Navbar.js
+++ client/src/componets/Navbar/Navbar.js
@@ -1,131 +1,77 @@
 import React from "react";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import Navbar from 'react-bootstrap/Navbar';
+import { Nav, Container, Offcanvas } from 'react-bootstrap'
+import './Navbar.css';
 
-<<<<<<< HEAD
-function Navbar({ currentPage, handlePageChange }) {
-  return (
-    <div classname="navBarContainer">
-      <Navbar bg="light" variant="light" fixed="top" expand={false}>
-        <Container fluid>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="../public/classifyLogo.png"
-              width="80"
-              height="80"
-              className="d-inline-block align-top"
-              />{' '}
-            classify
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="offcanvasNavbar" />
-          <Navbar.Offcanvas
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-            placement="end"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel"></Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="me-auto">
-                <Nav.Link 
-                  href="#schools"
-                  onClick={() => handlePageChange("Schools")}
-                  className={
-                  currentPage === "Schools" ? "nav-link active" : "nav-link"
-                  }>
-                    Schools
-                </Nav.Link>
-                <Nav.Link 
-                  href="#departments"
-                  onClick={() => handlePageChange("Departments")}
-                  className={
-                    currentPage === "Departments" ? "nav-link active" : "nav-link"
-                  }>
-                    Departments
-                </Nav.Link>
-                <Nav.Link 
-                  href="#students"
-                  onClick={() => handlePageChange("Students")}
-                  className={
-                    currentPage === "Students" ? "nav-link active" : "nav-link"
-                  }>
-                    Students
-                </Nav.Link>
-                <Nav.Link 
-                  href="#logout"
-                  onClick={() => handlePageChange("Logout")}
-                  className={
-                    currentPage === "Logout" ? "nav-link active" : "nav-link"
-                  }>
-                    Logout
-                </Nav.Link>
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    </div>
-  );
-=======
 function NavbarComp({ currentPage, handlePageChange }) {
 	return (
 		<div classname="navBarContainer">
-			<Navbar bg="light" variant="light">
+			<Navbar bg="light" variant="light" fixed="top">
 				<Container>
 					<Navbar.Brand href="#home">
 						<img
 							alt=""
 							src="../public/classifyLogo.png"
-							width="80"
+							width="auto"
 							height="80"
 							className="d-inline-block align-top"
 						/>{" "}
 						classify
 					</Navbar.Brand>
-					<Nav className="me-auto">
-						<Nav.Link
-							href="#schools"
-							onClick={() => handlePageChange("Schools")}
-							className={
-								currentPage === "Schools" ? "nav-link active" : "nav-link"
-							}
+					<Navbar.Toggle aria-controls="offcanvasNavbar" />
+						<Navbar.Offcanvas
+						id="offcanvasNavbar"
+						aria-labelledby="offcanvasNavbarLabel"
+						placement="end"
 						>
-							Schools
-						</Nav.Link>
+						<Offcanvas.Header closeButton>
+							<Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+						</Offcanvas.Header>
+						<Offcanvas.Body>
+							<Nav className="me-auto">
+								<Nav.Link
+									href="#schools"
+									onClick={() => handlePageChange("Schools")}
+									className={
+										currentPage === "Schools" ? "nav-link active" : "nav-link"
+									}
+								>
+									Schools
+								</Nav.Link>
 
-						<Nav.Link
-							href="#departments"
-							onClick={() => handlePageChange("Departments")}
-							className={
-								currentPage === "Departments" ? "nav-link active" : "nav-link"
-							}
-						>
-							Departments
-						</Nav.Link>
-						<Nav.Link
-							href="#students"
-							onClick={() => handlePageChange("Students")}
-							className={
-								currentPage === "Students" ? "nav-link active" : "nav-link"
-							}
-						>
-							Students
-						</Nav.Link>
-						<Nav.Link
-							href="#logout"
-							onClick={() => handlePageChange("Logout")}
-							className={
-								currentPage === "Logout" ? "nav-link active" : "nav-link"
-							}
-						>
-							Logout
-						</Nav.Link>
-					</Nav>
+								<Nav.Link
+									href="#departments"
+									onClick={() => handlePageChange("Departments")}
+									className={
+										currentPage === "Departments" ? "nav-link active" : "nav-link"
+									}
+								>
+									Departments
+								</Nav.Link>
+								<Nav.Link
+									href="#students"
+									onClick={() => handlePageChange("Students")}
+									className={
+										currentPage === "Students" ? "nav-link active" : "nav-link"
+									}
+								>
+									Students
+								</Nav.Link>
+								<Nav.Link
+									href="#logout"
+									onClick={() => handlePageChange("Logout")}
+									className={
+										currentPage === "Logout" ? "nav-link active" : "nav-link"
+									}
+								>
+									Logout
+								</Nav.Link>
+							</Nav>
+						</Offcanvas.Body>
+					</Navbar.Offcanvas>
 				</Container>
 			</Navbar>
 		</div>
 	);
->>>>>>> main
 }
-export default Navbar;
+export default NavbarComp;

2431768d7620fc6370945f4b713bd6673445fda0
LilTzar <eeoerkeedu@gmail.com>
2022-04-20 10:15:10 -0600

    continued building of schools page

--- client/src/componets/School/School.js
+++ client/src/componets/School/School.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 
 import { Auth } from "../../utils/auth";
 import { QUERY_SCHOOL } from "../../utils/queries";
 import { ADD_SCHOOL, UPDATE_SCHOOL, REMOVE_SCHOOL } from "../../utils/mutation";
+import { Button } from "react-bootstrap";
 
 function Schools() {
 	return (
 		<>
-			<div className="schoolInfoContainer">
-				<div id="schooloptions">
-					{params.Schools.map((school) => (
-						<option key={school} value={school}>
-							{school}
-						</option>
-					))}
-				</div>
-			</div>
+			<section className="schoolsContainer">
+				{console.log(state)}
+				{state.Schools.map((school) => (
+					<div key={school.id} id={school.id} className="card mb-3">
+						<h4 className="card-header bg-primary text-light p-2 m-0">
+							{school.name}
+						</h4>
+						<div className="card-body bg-light p-2">
+							<p>{school.principle}</p>
+							<code>
+								Assigned Budget:
+								{school.budget}
+							</code>
+							<code>
+								Departments:
+								{school.department}
+							</code>
+							<code>
+								Teachers:
+								{school.teachers}
+							</code>
+						</div>
+						<span style={{ fontSize: "1rem" }}></span>
+					</div>
+				))}
+			</section>
 		</>
 	);
 }

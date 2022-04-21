0e7187dbe1700fbc4f48362fa117c0ccc5d5153c
Michael Bee <bee.michael.02@gmail.com>
2022-04-21 15:04:25 -0500

    Footer changes.

--- client/src/componets/Footer/Footer.js
+++ client/src/componets/Footer/Footer.js
@@ -2,34 +2,36 @@ import React from "react";
 import './Footer.css';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
+import GitHubLogo from "../../img/githubLogoWhite.png"
 function Footer() {
 	return (
 		<footer class="footer">
 			<Row class="d-flex justify-content-center">
-				<Col class="col-4">
-				<a href="https://github.com/dkcook90/Classify_App"><img alt=""
-					src="../../public/githubLogoWhite.png"
-					width="80"
-					height="80"
-					className="d-inline-block align-center"
-					/></a>
+				<Col className="col-6">
+					<p>This project was created by the Classify Team:</p>
 				</Col>
-				<Col class="col-8">
-					<p>This project was created by the Classify Team. Find our repo @ GitHub.</p>
+				<Col>
+					<p>Find the Classify repo @ GitHub.</p>
 				</Col>
 			</Row>
-			<Row class="d-flex justify-content-center">
-				<p>Erik Oerke, Rick Morrissette, Donald Cook, Michael Bee</p>	
+			<Row className="my-2">
+				<Col className="col-6">
+					<p><a href="https://github.com/eeoerkeedu" className="teamLinks" target="_blank">Erik Oerke</a></p>
+					<p><a href="https://github.com/confusedicarus" className="teamLinks" target="_blank">Rick Morrissette</a></p> 
+					<p><a href="https://github.com/dkcook90" className="teamLinks" target="_blank">Donald Cook</a></p>
+					<p><a href="https://github.com/Michael-Bee" className="teamLinks" target="_blank">Michael Bee</a></p>
+				</Col>
+				<Col><a href="https://github.com/dkcook90/Classify_App" target="_blank"><img alt=""
+						src={GitHubLogo}
+						width="80"
+						height="80"
+						className="d-inline-block align-center"
+						/>
+					</a>
+				</Col>
+			</Row>
+			<Row>	
 				<p><span className="copyright">© Copyright 2022 Classify. All Rights Reserved.</span></p>
-				<img
-					alt=""
-					src="../../public/classifyLogo.png"
-					width="80"
-					height="80"
-					className="d-inline-block align-center"
-				/>
-				
 			</Row>
 		</footer>
 	);

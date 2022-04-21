0a21ba9457032df08b514c677c73ba7a64c00f21
Michael Bee <bee.michael.02@gmail.com>
2022-04-21 10:08:48 -0500

    Update to footer to include LinkedIn links.

--- client/src/componets/Footer/Footer.js
+++ client/src/componets/Footer/Footer.js
@@ -16,11 +16,15 @@ function Footer() {
 					/></a>
 				</Col>
 				<Col class="col-8">
-					<p>This project was created by the Classify Team. Find our repo @ GitHub.</p>
+					<p>This project was created by the Classify Team.</p> 
+					<p><a href="https://www.linkedin.com/in/erik-oerke-6a24bb8b/">Erik Oerke</a></p>
+					<p><a href="https://www.linkedin.com/in/rick-morrissette-4bb362149/">Rick Morrissette</a></p>
+					<p><a href="https://www.linkedin.com/in/donald-cook-4aa46813b/">Donald Cook</a></p>
+					<p><a href="https://www.linkedin.com/in/michael-bee-13676a225/">Michael Bee</a></p>
+					<p>Find our Classify repo @ GitHub.</p>
 				</Col>
 			</Row>
-			<Row class="d-flex justify-content-center">
-				<p>Erik Oerke, Rick Morrissette, Donald Cook, Michael Bee</p>	
+			<Row class="d-flex justify-content-center">	
 				<p><span className="copyright">© Copyright 2022 Classify. All Rights Reserved.</span></p>
 				<img
 					alt=""
@@ -29,7 +33,6 @@ function Footer() {
 					height="80"
 					className="d-inline-block align-center"
 				/>
-				
 			</Row>
 		</footer>
 	);

f155017e7482e5e8a29fd1e9238747242ed0bf40
Michael Bee <bee.michael.02@gmail.com>
2022-04-22 11:27:13 -0500

    Making footer always at bottom regardless of body size.

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
+        <div className="d-flex flex-column justify-flex-start min-100-vh">
           <NavbarComp />
           <div className="container">
             <Routes>

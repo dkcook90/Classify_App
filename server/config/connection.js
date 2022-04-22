8a6405efc3044f6492ebe74dffc48a719b571a7b
Rick <95931094+confusedicarus@users.noreply.github.com>
2022-04-22 16:39:00 -0600

    correction

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/classify_app',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/classify-app',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,

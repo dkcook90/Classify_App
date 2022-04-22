63ad239db8cc1a91913f4b67db60b4d91dbf29ae
Rick <95931094+confusedicarus@users.noreply.github.com>
2022-04-22 15:34:31 -0600

    add department button still needs work

--- client/src/componets/Department/Department.js
+++ client/src/componets/Department/Department.js
@@ -3,12 +3,12 @@ import { Form, Button, Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Department.css";
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_SCHOOL } from "../../utils/queries";
-import { ADD_DEPT_SCHOOL } from "../../utils/mutation";
+import { ADD_DEPT_SCHOOL, ADD_DEPT } from "../../utils/mutation";
 import { useParams, Link } from "react-router-dom";
 
 function Department() {
   let { id } = useParams();
-  const { loading, error, data } = useQuery(QUERY_SCHOOL, {
+  const { loading, data } = useQuery(QUERY_SCHOOL, {
     variables: { _id: id },
   });
   const school = data?.school || [];
@@ -17,27 +17,38 @@ function Department() {
   console.log(deptList);
 
   const [departmentState, setDepartmentState] = useState({
-    schoolId: "",
-    departmentId: "",
+    department: "",
   });
-  const addDepartment = useMutation(ADD_DEPT_SCHOOL);
+  const [addDepartment, { error }] = useMutation(ADD_DEPT);
   const handleChange = (e) => {
-    const { id, value } = e.target;
+    const { department, value } = e.target;
     setDepartmentState({
       ...departmentState,
-      [id]: value,
+      [department]: value,
     });
   };
 
-  const handleFormSubmit = async (e) =>{
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-  }
-  console.log(departmentState)
+    console.log("YOOO");
+    try {
+      const { data } = await addDepartment({
+        variable: { ...departmentState },
+      });
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  console.log(departmentState);
   return (
     <>
       <div className="departmentContainer">
         <Card style={{ width: "18rem" }}>
-          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+          <Card.Img
+            variant="top"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Camberwell_High_School.jpg/1200px-Camberwell_High_School.jpg"
+          />
           <Card.Body>
             <Card.Title>{school.name}</Card.Title>
             <Card.Text>
@@ -49,8 +60,8 @@ function Department() {
               {deptList ? (
                 school.department.map((school) => {
                   return (
-                    <ListGroupItem>
-                      <Card.Link key={school.department._id} href="#">{school.department}</Card.Link>
+                    <ListGroupItem key={school.department._id}>
+                      <Card.Link href="#">{school.department}</Card.Link>
                     </ListGroupItem>
                   );
                 })
@@ -67,20 +78,22 @@ function Department() {
           </Card.Body>
         </Card>
 
-        <Form className="departmentForm">
+        <Form className="departmentForm" onSubmit={handleFormSubmit}>
           <Form.Group className="m-3" controlId="form">
             <Form.Label>Department Name:</Form.Label>
             <Form.Control
               className="mb-2"
               type="input"
               placeholder="Department Name"
+              value={departmentState}
+              onChange={(e) => setDepartmentState(e.target.value)}
             />
             <Button variant="secondary" type="submit">
               ADD DEPARTMENT
             </Button>
-            <Button variant="secondary" type="submit">
+            {/* <Button variant="secondary" type="submit">
               EDIT DEPARTMENT
-            </Button>
+            </Button> */}
           </Form.Group>
         </Form>
       </div>
--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,28 +1,28 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-	mutation loginUser($email: String!, $password: String!) {
-		login(email: $email, password: $password) {
-			token
-			user {
-				_id
-				username
-				email
-			}
-		}
-	}
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
 `;
 
 export const ADD_USER = gql`
-	mutation addUser($username: String!, $email: String!, $password: String!) {
-		addUser(username: $username, email: $email, password: $password) {
-			token
-			user {
-				_id
-				username
-			}
-		}
-	}
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 // export const REMOVE_USER = gql`
@@ -33,65 +33,74 @@ export const ADD_USER = gql`
 // `;
 
 export const ADD_SCHOOL = gql`
-	mutation addSchool($name: String!) {
-		addSchool(name: $name) {
-			_id
-			name
-			principle
-			budget
-			department
-			teachers
-		}
-	}
+  mutation addSchool($name: String!) {
+    addSchool(name: $name) {
+      _id
+      name
+      principle
+      budget
+      department
+      teachers
+    }
+  }
 `;
 
 export const UPDATE_SCHOOL = gql`
-	mutation updateSchool($name: String!) {
-		updateSchool(name: $name) {
-			_id
-			name
-			principle
-			budget
-			department
-			teachers
-		}
-	}
+  mutation updateSchool($name: String!) {
+    updateSchool(name: $name) {
+      _id
+      name
+      principle
+      budget
+      department
+      teachers
+    }
+  }
 `;
 
 export const REMOVE_SCHOOL = gql`
-	mutation removeSchool($schoolId: ID!) {
-		removeSchool(schoolId: $schoolId) {
-			_id
-			name
-			principle
-			budget
-		}
-	}
+  mutation removeSchool($schoolId: ID!) {
+    removeSchool(schoolId: $schoolId) {
+      _id
+      name
+      principle
+      budget
+    }
+  }
+`;
+export const ADD_DEPT = gql`
+  mutation addDept($department: String!) {
+    addDepartment(department: $department) {
+      _id
+      department
+    }
+  }
 `;
 
 export const ADD_DEPT_SCHOOL = gql`
-	mutation addDepToSchool($schoolId: String!, $departmentId: String!){
-		addDeptToSchool(schoolId: $schoolId, departmentId: $departmentId){
-			_id
-			name
-			principle
-			budget
-			department{
-				_id
-				department
-				budget
-			}
-			teachers{
-				_id
-				name
-				department
-				office
-				students{
-					_id
-					name
-					grade
-					note
-				}
-			}
-		}
-	}`
+  mutation addDepToSchool($schoolId: String!, $departmentId: String!) {
+    addDeptToSchool(schoolId: $schoolId, departmentId: $departmentId) {
+      _id
+      name
+      principle
+      budget
+      department {
+        _id
+        department
+        budget
+      }
+      teachers {
+        _id
+        name
+        department
+        office
+        students {
+          _id
+          name
+          grade
+          note
+        }
+      }
+    }
+  }
+`;

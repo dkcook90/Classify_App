a90f2e2e62a5df942767ffe463c1ade25bc4dc88
Michael Bee <bee.michael.02@gmail.com>
2022-04-22 09:28:08 -0500

    Footer changes.

--- client/src/componets/Footer/Footer.js
+++ client/src/componets/Footer/Footer.js
@@ -6,22 +6,23 @@ import GitHubLogo from "../../img/githubLogoWhite.png"
 function Footer() {
 	return (
 		<footer class="footer">
-			<Row class="d-flex justify-content-center">
-				<Col className="col-6">
+			<Row className="justify-content-md-center">
+				<Col>
 					<p>This project was created by the Classify Team:</p>
 				</Col>
 				<Col>
 					<p>Find the Classify repo @ GitHub.</p>
 				</Col>
 			</Row>
-			<Row className="my-2">
-				<Col className="col-6">
+			<Row className="justify-content-md-center my-2">
+				<Col>
 					<p><a href="https://github.com/eeoerkeedu" className="teamLinks" target="_blank">Erik Oerke</a></p>
 					<p><a href="https://github.com/confusedicarus" className="teamLinks" target="_blank">Rick Morrissette</a></p> 
 					<p><a href="https://github.com/dkcook90" className="teamLinks" target="_blank">Donald Cook</a></p>
 					<p><a href="https://github.com/Michael-Bee" className="teamLinks" target="_blank">Michael Bee</a></p>
 				</Col>
-				<Col><a href="https://github.com/dkcook90/Classify_App" target="_blank"><img alt=""
+				<Col>
+					<a href="https://github.com/dkcook90/Classify_App" target="_blank"><img alt=""
 						src={GitHubLogo}
 						width="80"
 						height="80"
@@ -30,8 +31,10 @@ function Footer() {
 					</a>
 				</Col>
 			</Row>
-			<Row>	
-				<p><span className="copyright">© Copyright 2022 Classify. All Rights Reserved.</span></p>
+			<Row className="justify-content-md-center">	
+				<Col>
+					<p><span className="copyright">© Copyright 2022 Classify. All Rights Reserved.</span></p>
+				</Col>
 			</Row>
 		</footer>
 	);

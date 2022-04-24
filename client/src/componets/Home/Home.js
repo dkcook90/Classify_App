b9162d550dc3c97be40b016e6f39fd0ecc5ecb8c
Michael Bee <bee.michael.02@gmail.com>
2022-04-24 14:20:50 -0500

    Styling to Home Welcome page.

--- client/src/componets/Home/Home.js
+++ client/src/componets/Home/Home.js
@@ -9,41 +9,42 @@ import Auth from "../../utils/auth";
 function Home() {
 	return (
 		<>
-			<div className="m-3 Container">
+			<div>
 			{Auth.loggedIn() ? (
 				<>
-				<Image
-					className="homeBackground"
-					src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2Nob29sfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-					fluid="true"
-				></Image>
-				<h1>Welcome to District 925!</h1>
-				<ButtonGroup className="mb-2">
-					<Button variant="primary">
-						<Link className="backHome" to={"/schools"}>
-							Schools
-						</Link>
-					</Button>
-					<Button variant="primary">
-						<Link className="backHome" to={"/departments"}>
-							Departments
-						</Link>
-					</Button>
-					<Button variant="primary">
-						<Link className="backHome" to={"/classroom"}>
-							Classrooms
-						</Link>
-					</Button>
-				</ButtonGroup>
-
-				<Form>
+				<container className="justify-content-center text-center container row">
+					<Image
+						className="homeBackground"
+						src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2Nob29sfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+						fluid="true"
+					></Image>
+					<ButtonGroup className="mb-2">
+						<Button variant="primary" className="groupbtn">
+							<Link className="backHome" to={"/schools"}>
+								Schools
+							</Link>
+						</Button>
+						<Button variant="primary" className="groupbtn">
+							<Link className="backHome" to={"/departments"}>
+								Departments
+							</Link>
+						</Button>
+						<Button variant="primary" className="groupbtn">
+							<Link className="backHome" to={"/classroom"}>
+								Classrooms
+							</Link>
+						</Button>
+					</ButtonGroup>
+					<h1>Welcome to School District 925!</h1>
+				</container>
+				<Form className="p-3">
 					<Form.Group className="mb-3" controlId="formSearch">
-						<Form.Label>
+						<Form.Label className="justify-content-center text-center container row">
 							Enter name of Student or Teacher you would like to view.
 						</Form.Label>
 						<Form.Control type="search" placeholder="Search" />
 					</Form.Group>
-					<Form.Group className="mb-3" controlId="formBasicCheckbox">
+					<Form.Group className="mb-3">
 						<Form.Check type="checkbox" label="Student" />
 						<Form.Check type="checkbox" label="Teacher" />
 					</Form.Group>
